b9a18bf27bd7ecd407fb8379307f025b8aaa566c
VovaLyapota <rimidalvatopayl@gmail.com>
2024-08-23 10:11:47 +0300

    add supplier methods to interact with db

--- src/suppliers/suppliers.service.ts
+++ src/suppliers/suppliers.service.ts
@@ -8,4 +8,25 @@ export class SuppliersService {
   constructor(
     @InjectRepository(Supplier) private suppliersRepo: Repository<Supplier>,
   ) {}
+
+  async findOne(id: number) {
+    return await this.suppliersRepo.findOneBy({ id });
+  }
+
+  async findAll() {
+    return await this.suppliersRepo.find();
+  }
+
+  async create(supplierData: Supplier) {
+    const supplier = this.suppliersRepo.create(supplierData);
+
+    return await this.suppliersRepo.save(supplier);
+  }
+
+  async update(id: number, updateValues: Partial<Supplier>) {
+    const supplier = await this.findOne(id);
+    Object.assign(supplier, updateValues);
+
+    return await this.suppliersRepo.save(supplier);
+  }
 }
