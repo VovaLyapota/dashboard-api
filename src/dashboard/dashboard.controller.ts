ea5e526485615c04181b32c8d52d78e0bc5684b5
VovaLyapota <rimidalvatopayl@gmail.com>
2024-09-02 17:04:44 +0300

    add dashboard controller

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CustomersService } from 'src/customers/customers.service';
+import { ProductsService } from 'src/products/products.service';
+import { SuppliersService } from 'src/suppliers/suppliers.service';
 
 @Controller('dashboard')
-export class DashboardController {}
+export class DashboardController {
+  constructor(
+    private productsService: ProductsService,
+    private suppliersService: SuppliersService,
+    private customersService: CustomersService,
+  ) {}
+
+  @Get()
+  async getDashboard() {
+    const [products, suppliers, customers] = await Promise.all([
+      this.productsService.count(),
+      this.suppliersService.count(),
+      this.customersService.count(),
+    ]);
+
+    return {
+      products,
+      suppliers,
+      customers,
+    };
+  }
+}
