e0f93e88c3712a68d8b9e584aeac5f32d5bd9219
VovaLyapota <rimidalvatopayl@gmail.com>
2024-09-02 10:06:24 +0300

    add customer CRUD operations to controller

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,63 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CustomersService } from './customers.service';
+import { GetCustomersDto } from './dtos/get-customers.dto';
+import { CreateCustomerDto } from './dtos/create-customer.dto';
+import { UpdateCustomerDto } from './dtos/update-customer.dto';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+  constructor(private customersService: CustomersService) {}
+
+  @Get()
+  async getCustomers(@Query() query: GetCustomersDto) {
+    const customers = await this.customersService.findAll(query);
+    if (!customers.length)
+      throw new NotFoundException('There are no any customers.');
+
+    return customers;
+  }
+
+  @Get('/:id')
+  async getOneCustomer(@Param('id') id: string) {
+    if (!+id) throw new BadRequestException('Invalid id property');
+
+    const customer = await this.customersService.findOne(+id);
+    if (!customer)
+      throw new NotFoundException('There is no customer with such an id');
+
+    return customer;
+  }
+
+  @Post()
+  createCustomer(@Body() body: CreateCustomerDto) {
+    return this.customersService.create(body);
+  }
+
+  @Put('/:id')
+  updateCustomer(@Param('id') id: string, @Body() body: UpdateCustomerDto) {
+    if (!+id) throw new BadRequestException('Invalid id property');
+
+    return this.customersService.update(+id, body);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteCustomer(@Param('id') id: string) {
+    if (!+id) throw new BadRequestException('Invalid id property');
+
+    return this.customersService.delete(+id);
+  }
+}
