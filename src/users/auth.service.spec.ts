f247bd4349e31e98d0df424a630af80098fc65ec
VovaLyapota <rimidalvatopayl@gmail.com>
2024-09-03 17:08:41 +0300

    add userService tests, start doing auth tests (unit tests)

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,15 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from './users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let usersServiceMok: UsersService;
+  let jwtServiceMok: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: JwtService, useValue: {} },
+        { provide: UsersService, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    usersServiceMok = module.get<UsersService>(UsersService);
+    jwtServiceMok = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
