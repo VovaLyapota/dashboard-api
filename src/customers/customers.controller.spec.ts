e6399fd4000491bcfc7aafc3fecfec2b9605c5a6
VovaLyapota <rimidalvatopayl@gmail.com>
2024-09-05 18:42:43 +0300

    add tests for customers module

--- src/customers/customers.controller.spec.ts
+++ src/customers/customers.controller.spec.ts
@@ -1,18 +1,133 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersController } from './customers.controller';
+import { CustomersService } from './customers.service';
+import { Customer } from './customer.entity';
+import { GetCustomersDto } from './dtos/get-customers.dto';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { CreateCustomerDto } from './dtos/create-customer.dto';
+
+class CustomersServiceMock {
+  findAll = jest.fn();
+  findOne = jest.fn();
+  create = jest.fn();
+  update = jest.fn();
+  delete = jest.fn();
+}
 
 describe('CustomersController', () => {
   let controller: CustomersController;
+  let customersServiceMock: CustomersServiceMock;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CustomersController],
+      providers: [
+        {
+          provide: CustomersService,
+          useClass: CustomersServiceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<CustomersController>(CustomersController);
+    customersServiceMock = module.get<CustomersServiceMock>(CustomersService);
   });
 
-  it('should be defined', () => {
+  it('customers controller should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it("getCustomers - returns customer's list by given options", async () => {
+    customersServiceMock.findAll.mockResolvedValueOnce([{}] as Customer[]);
+    const foundCustomers = await controller.getCustomers({} as GetCustomersDto);
+
+    expect(foundCustomers).toEqual([{}] as Customer[]);
+    expect(customersServiceMock.findAll).toHaveBeenCalled();
+  });
+
+  it('getCustomers - throws a NotFoundException if no customers were found', async () => {
+    customersServiceMock.findAll.mockResolvedValueOnce([]);
+
+    await expect(
+      controller.getCustomers({} as GetCustomersDto),
+    ).rejects.toThrow(NotFoundException);
+    expect(customersServiceMock.findAll).toHaveBeenCalled();
+  });
+
+  it('getOneCustomer - returns the customer by given id', async () => {
+    customersServiceMock.findOne.mockResolvedValueOnce({} as Customer);
+    const customer = await controller.getOneCustomer('1');
+
+    expect(customer).toEqual({} as Customer);
+    expect(customersServiceMock.findOne).toHaveBeenCalledWith(1);
+  });
+
+  it('getOneCustomer - throws a BadRequestException if invalid id was given', async () => {
+    await expect(controller.getOneCustomer('invalid_id')).rejects.toThrow(
+      BadRequestException,
+    );
+    expect(customersServiceMock.findOne).not.toHaveBeenCalled();
+  });
+
+  it('getOneCustomer - throws a NotFoundException if no user by given id was found', async () => {
+    customersServiceMock.findOne.mockResolvedValueOnce(null);
+
+    await expect(controller.getOneCustomer('1')).rejects.toThrow(
+      NotFoundException,
+    );
+    expect(customersServiceMock.findOne).toHaveBeenCalled();
+  });
+
+  it('createCustomer - creates and returns a customer by given dto', async () => {
+    customersServiceMock.create.mockResolvedValueOnce({} as Customer);
+    const createdCustomer = await controller.createCustomer(
+      {} as CreateCustomerDto,
+    );
+
+    expect(createdCustomer).toEqual({} as Customer);
+    expect(customersServiceMock.create).toHaveBeenCalled();
+  });
+
+  it('updateCustomer - updates and returns a customer by given id and update values', async () => {
+    const updateCustomerDto = { spent: 1231 };
+    customersServiceMock.update.mockResolvedValueOnce({
+      ...({} as Customer),
+      ...updateCustomerDto,
+    });
+    const updatedCustomer = await controller.updateCustomer(
+      '1',
+      updateCustomerDto,
+    );
+
+    expect(updatedCustomer).toEqual({
+      ...({} as Customer),
+      ...updateCustomerDto,
+    });
+    expect(customersServiceMock.update).toHaveBeenCalledWith(
+      1,
+      updateCustomerDto,
+    );
+  });
+
+  it('updateCustomer - throws a BadRequestException if invalid id was given', async () => {
+    // await expect(controller.updateCustomer('invalid_id', {})).rejects.toThrow(
+    //   BadRequestException,
+    // );
+    expect(customersServiceMock.update).not.toHaveBeenCalled();
+  });
+
+  it('deleteCustomer - deletes a customer by given id', async () => {
+    customersServiceMock.delete.mockResolvedValueOnce({} as Customer);
+    const res = await controller.deleteCustomer('1');
+
+    expect(res).toBeUndefined();
+    expect(customersServiceMock.delete).toHaveBeenCalledWith(1);
+  });
+
+  it('deleteCustomer - throws a BadRequestException if invalid id was given', async () => {
+    // await expect(controller.deleteCustomer('1')).rejects.toThrow(
+    //   BadRequestException,
+    // );
+    expect(customersServiceMock.delete).not.toHaveBeenCalled();
   });
 });
--- src/customers/customers.service.spec.ts
+++ src/customers/customers.service.spec.ts
@@ -1,18 +1,131 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersService } from './customers.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Customer } from './customer.entity';
+import { GetCustomersDto } from './dtos/get-customers.dto';
+import { CreateCustomerDto } from './dtos/create-customer.dto';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+
+class CustomersRepoMock {
+  findOneBy = jest.fn();
+  count = jest.fn();
+  create = jest.fn();
+  save = jest.fn();
+  remove = jest.fn();
+  createQueryBuilder = jest.fn().mockReturnValue({
+    where: jest.fn().mockReturnThis(),
+    andWhere: jest.fn().mockReturnThis(),
+    getMany: jest.fn(),
+  });
+}
 
 describe('CustomersService', () => {
   let service: CustomersService;
+  let customersRepoMock: CustomersRepoMock;
+  let customer: Customer;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CustomersService],
+      providers: [
+        CustomersService,
+        {
+          provide: getRepositoryToken(Customer),
+          useClass: CustomersRepoMock,
+        },
+      ],
     }).compile();
 
     service = module.get<CustomersService>(CustomersService);
+    customersRepoMock = module.get<CustomersRepoMock>(
+      getRepositoryToken(Customer),
+    );
+    customer = {
+      id: 1,
+      image: null,
+      name: 'Customer',
+      email: 'customer@gmail.com',
+      spent: 129,
+      phone: '+38000000000',
+      address: 'address',
+      registeredAt: '01-01-2021',
+    } as Customer;
   });
 
-  it('should be defined', () => {
+  it('customers service should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findOne - returns the customer by given id', async () => {
+    customersRepoMock.findOneBy.mockResolvedValueOnce(customer);
+    const foundCustomer = await service.findOne(1);
+
+    expect(foundCustomer).toEqual(customer);
+    expect(customersRepoMock.findOneBy).toHaveBeenCalledWith({ id: 1 });
+  });
+
+  it('findAll - returns all the customers by given options', async () => {
+    customersRepoMock
+      .createQueryBuilder()
+      .getMany.mockResolvedValueOnce([customer]);
+    const foundCustomers = await service.findAll({} as GetCustomersDto);
+
+    expect(foundCustomers).toEqual([customer]);
+    expect(customersRepoMock.createQueryBuilder).toHaveBeenCalled();
+  });
+
+  it('count - returns a number of the whole customers', async () => {
+    customersRepoMock.count.mockResolvedValueOnce(3);
+    const numberOfCustomers = await service.count();
+
+    expect(numberOfCustomers).toEqual(3);
+    expect(customersRepoMock.count).toHaveBeenCalled();
+  });
+
+  it('create - creates and returns a customer by given dto', async () => {
+    customersRepoMock.create.mockReturnValueOnce(customer);
+    customersRepoMock.save.mockResolvedValueOnce(customer);
+    const createdCustomer = await service.create({} as CreateCustomerDto);
+
+    expect(createdCustomer).toEqual(customer);
+    expect(customersRepoMock.create).toHaveBeenCalled();
+    expect(customersRepoMock.save).toHaveBeenCalledWith(customer);
+  });
+
+  it('update - updates and returns a customer by given id and update dto', async () => {
+    const updateCustomerDto = { spent: 500 };
+    customersRepoMock.findOneBy.mockResolvedValueOnce(customer);
+    customersRepoMock.save.mockResolvedValueOnce({
+      ...customer,
+      ...updateCustomerDto,
+    });
+    const updatedCustomer = await service.update(1, updateCustomerDto);
+
+    expect(updatedCustomer).toEqual({ ...customer, ...updateCustomerDto });
+    expect(customersRepoMock.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    expect(customersRepoMock.save).toHaveBeenCalled();
+  });
+
+  it('update - throws a NotFoundException if no customer was found by given id', async () => {
+    customersRepoMock.findOneBy.mockResolvedValueOnce(null);
+
+    await expect(service.update(1, {})).rejects.toThrow(NotFoundException);
+    expect(customersRepoMock.save).not.toHaveBeenCalled();
+  });
+
+  it('delete - deletes a customer by given id', async () => {
+    customersRepoMock.findOneBy.mockResolvedValueOnce(customer);
+    customersRepoMock.remove.mockResolvedValueOnce(customer);
+    const deletedCustomer = await service.delete(1);
+
+    expect(deletedCustomer).toEqual(customer);
+    expect(customersRepoMock.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    expect(customersRepoMock.remove).toHaveBeenCalledWith(customer);
+  });
+
+  it('delete - throws a BadRequestException if invalid id was given', async () => {
+    customersRepoMock.findOneBy.mockResolvedValueOnce(null);
+
+    await expect(service.delete(1)).rejects.toThrow(BadRequestException);
+    expect(customersRepoMock.remove).not.toHaveBeenCalled();
   });
 });
